93c02389a23da7339e7c956d27d948ffa2403519
JUSTIN YE <justinye@college.harvard.edu>
2021-10-01 11:43:27 -0400

    Basic list of countries on home page, text only

--- lib/sections/home.dart
+++ lib/sections/home.dart
@@ -3,12 +3,43 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 200.0),
         child: Center(
-            child: Column(
+            child: ListView(
+      children: [
+        CountryTag("South Asia"),
+        CountryTag("Bangladesh"),
+        CountryTag("Afghanistan"),
+      ],
+    )));
+  }
+}
+
+class CountryTag extends StatefulWidget {
+  String _country = "";
+
+  CountryTag(String country) {
+    this._country = country;
+  }
+
+  @override
+  _CountryTagState createState() => _CountryTagState(this._country);
+}
+
+class _CountryTagState extends State<CountryTag> {
+  String _country = "";
+
+  _CountryTagState(String country) {
+    this._country = country;
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+        child: Center(
+            child: Row(
           children: [
-            Text("This is the Home Page", textScaleFactor: 2),
-            Icon(Icons.home, size: 150, color: Colors.blue),
+            Text("FLAG GOES HERE", textScaleFactor: 2),
+            Text(_country, textScaleFactor: 2)
           ],
         )));
   }
