ce69bbb11281c5c32d853bf021e5248bbcc3e255
JUSTIN YE <justinye@college.harvard.edu>
2021-10-03 10:27:22 -0400

    Homepage UI without search bar

--- lib/sections/home.dart
+++ lib/sections/home.dart
@@ -6,9 +6,9 @@ class HomePage extends StatelessWidget {
         child: Center(
             child: ListView(
       children: [
-        CountryTag("South Asia"),
-        CountryTag("Bangladesh"),
-        CountryTag("Afghanistan"),
+        CountryTag("Bhutan", 'assets/flags/Bhutan.png'),
+        CountryTag("Bangladesh", 'assets/flags/Bangladesh.png'),
+        CountryTag("Afghanistan", 'assets/flags/Afghanistan.jpg'),
       ],
     )));
   }
@@ -16,31 +16,47 @@ class HomePage extends StatelessWidget {
 
 class CountryTag extends StatefulWidget {
   String _country = "";
+  String _image = "";
 
-  CountryTag(String country) {
+  CountryTag(String country, String image) {
     this._country = country;
+    this._image = image;
   }
 
   @override
-  _CountryTagState createState() => _CountryTagState(this._country);
+  _CountryTagState createState() =>
+      _CountryTagState(this._country, this._image);
 }
 
 class _CountryTagState extends State<CountryTag> {
-  String _country = "";
+  String _country = "", _image = "";
 
-  _CountryTagState(String country) {
+  _CountryTagState(String country, String image) {
     this._country = country;
+    this._image = image;
   }
 
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-        child: Center(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        height: 100,
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.black,
+        )),
+        child: Align(
+            alignment: Alignment.center,
             child: Row(
-          children: [
-            Text("FLAG GOES HERE", textScaleFactor: 2),
-            Text(_country, textScaleFactor: 2)
-          ],
-        )));
+              children: [
+                Container(
+                  child: Image.asset(_image, fit: BoxFit.fitHeight, width: 90),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 0.0),
+                ),
+                Text(_country,
+                    textScaleFactor: 1.8, textAlign: TextAlign.center)
+              ],
+            )));
   }
 }
