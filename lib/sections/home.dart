ea02196bab5a84461719186de0b2b6188e3ab8ef
JUSTIN YE <justinye@college.harvard.edu>
2021-10-04 22:52:06 -0400

    Third layer

--- lib/sections/home.dart
+++ lib/sections/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// HOME PAGE
 // Home Page widget, stateless and holds country tags
 class HomePage extends StatelessWidget {
   // Return container with list of countries
@@ -120,6 +121,7 @@ class _CountryTagState extends State<CountryTag> {
   }
 }
 
+// FIRST LAYER
 // List of categories (eg. population)
 class CategoryPage extends StatelessWidget {
   // No state (like list of countries), return list of tags
@@ -163,9 +165,113 @@ class _CategoryTagState extends State<CategoryTag> {
           centerTitle: true,
         ),
         body: Center(
+            // Container with info on category
+            child: CategoryInfoPage()),
+        // Navbar
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.green,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.query_stats),
+              label: 'Compare',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.folder_open),
+              label: 'Reporting',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.more),
+              label: 'More',
+            ),
+          ],
+          // TODO: fix navbar if possible
+          // currentIndex: _selectedIndex,
+          // onTap: _onItemTapped,
+        ),
+      );
+    }));
+  }
+
+  // Tag with name of category, detects tap
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          _pushInfo();
+        },
+        child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            height: 100,
+            decoration: BoxDecoration(
+                border: Border.all(
+              color: Colors.black,
+            )),
+            child: Align(
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    Text(_category,
+                        textScaleFactor: 1.8, textAlign: TextAlign.center)
+                  ],
+                ))));
+  }
+}
+
+// SECOND LAYER
+// List of categories (eg. population)
+class CategoryInfoPage extends StatelessWidget {
+  // No state (like list of countries), return list of tags
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+            child: ListView(
+      children: [
+        CategoryInfo("Population Under 18"),
+        CategoryInfo("Population Over 65")
+      ],
+    )));
+  }
+}
+
+// Create tag listing a category
+// TODO: Convert to stateless if possible
+class CategoryInfo extends StatefulWidget {
+  String _statName = "";
+
+  CategoryInfo(String statName) {
+    this._statName = statName;
+  }
+
+  @override
+  _CategoryInfoState createState() => _CategoryInfoState(this._statName);
+}
+
+// State for category tag
+class _CategoryInfoState extends State<CategoryInfo> {
+  String _statName = "";
+
+  _CategoryInfoState(String statName) {
+    this._statName = statName;
+  }
+
+  // Function to push section for category
+  void _pushInfo() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(_statName),
+          centerTitle: true,
+        ),
+        body: Center(
           // Container with info on category
           child: Container(
-            child: Text(_category, textScaleFactor: 2),
+            child: Text(_statName, textScaleFactor: 2),
             color: Colors.lightGreen,
             padding:
                 const EdgeInsets.symmetric(horizontal: 80.0, vertical: 80.0),
@@ -219,7 +325,7 @@ class _CategoryTagState extends State<CategoryTag> {
                 alignment: Alignment.center,
                 child: Row(
                   children: [
-                    Text(_category,
+                    Text(_statName,
                         textScaleFactor: 1.8, textAlign: TextAlign.center)
                   ],
                 ))));
