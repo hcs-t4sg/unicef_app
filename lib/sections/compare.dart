e02cf1497de829205e21dcf6b2013cfcf702b768
ZhuMacPro <zhumacpro@dhcp-10-250-35-128.harvard.edu>
2021-10-31 16:42:21 -0400

    button

--- lib/sections/compare.dart
+++ lib/sections/compare.dart
@@ -7,8 +7,15 @@ class ComparePage extends StatelessWidget {
       body: Center(
         child: Container(
             child: ListView(children: <Widget>[
-          Container(child: dropDownMenu()),
-          Container(child: dropDownMenu()),
+          Container(child: dropDownData()),
+          Container(child: dropDownData()),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Select Countries'),
+              ))
         ])),
       ),
     );
@@ -16,15 +23,49 @@ class ComparePage extends StatelessWidget {
 }
 
 /// This is the stateful widget that the main application instantiates.
-class dropDownMenu extends StatefulWidget {
-  const dropDownMenu({Key? key}) : super(key: key);
+class dropDownData extends StatefulWidget {
+  const dropDownData({Key? key}) : super(key: key);
 
   @override
-  State<dropDownMenu> createState() => _dropDownMenuState();
+  State<dropDownData> createState() => _dropDownDataState();
 }
 
 /// This is the private State class that goes with MyStatefulWidget.
-class _dropDownMenuState extends State<dropDownMenu> {
+class _dropDownDataState extends State<dropDownData> {
+  String dropdownValue = 'SELECT DATA:';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.black),
+      underline: Container(
+        height: 2,
+        color: Colors.green,
+      ),
+      hint: Text("SELECT DATA:",
+          style: TextStyle(
+              color: Colors.black, fontSize: 14, fontWeight: FontWeight.w500)),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['SELECT DATA:', 'One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class _dropDownState extends State<dropDownData> {
   String dropdownValue = 'SELECT DATA:';
 
   @override
