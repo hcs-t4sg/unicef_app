804d81503c55f346614dc887518e7a926c80c78f
JUSTIN YE <justinye@college.harvard.edu>
2021-10-31 17:39:12 -0400

    dropdown test

--- lib/sections/compare/compare.dart
+++ lib/sections/compare/compare.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import './../../model.dart';
 
-class ComparePage extends StatelessWidget {
+class ComparePage extends StatefulWidget {
+  ComparePage({required this.title, required this.data});
+  String title;
+  List<Indicator> data;
+
+  @override
+  _ComparePageState createState() => _ComparePageState();
+}
+
+class _ComparePageState extends State<ComparePage> {
+  String dropdownValue = 'One';
+
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 200.0),
-        child: Center(
-            child: Column(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 200.0),
+      child: Center(
+        child: Column(
           children: [
             Text("This is the Compare Page", textScaleFactor: 2),
             Icon(Icons.folder_open, size: 150, color: Colors.blue),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['One', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            )
           ],
-        )));
+        ),
+      ),
+    );
   }
 }
