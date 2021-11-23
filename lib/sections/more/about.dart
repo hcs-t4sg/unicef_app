47b839caeae77acf95b6b528d23b5685cc9ad002
Matthew Su <matthew.su.2019@gmail.com>
2021-11-23 08:24:14 -0500

    removed T4sg paragraph and hyperlinks in More

--- lib/sections/more/about.dart
+++ lib/sections/more/about.dart
@@ -104,47 +104,47 @@ class AboutPage extends StatelessWidget {
                         fit: BoxFit.fitHeight,
                       ),
                     ),
-                    Container(
-                      child: Text(
-                        "Tech for Social Good",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                      child: Text(
-                        "This app was developed by Harvard Computer Society Tech for Social Good. T4SG leverages Harvard talent to partner with nonprofits, government agencies, and social impact organizations to amplify their impact through technology.",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400, fontSize: 12),
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
-                      padding: EdgeInsets.fromLTRB(10.0, 10, 10, 0.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border(
-                          top: BorderSide(color: Colors.grey, width: 0.5),
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            child: Icon(MdiIcons.facebook,
-                                color: Colors.blue, size: 30),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            child: Icon(MdiIcons.web,
-                                color: Colors.blue, size: 30),
-                          ),
-                        ],
-                      ),
-                    )
+                    // Container(
+                    //   child: Text(
+                    //     "Tech for Social Good",
+                    //     style: TextStyle(
+                    //         fontWeight: FontWeight.bold, fontSize: 20),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    //   child: Text(
+                    //     "This app was developed by Harvard Computer Society Tech for Social Good. T4SG leverages Harvard talent to partner with nonprofits, government agencies, and social impact organizations to amplify their impact through technology.",
+                    //     style: TextStyle(
+                    //         fontWeight: FontWeight.w400, fontSize: 12),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   width: double.infinity,
+                    //   margin:
+                    //       EdgeInsets.symmetric(horizontal: 0.0, vertical: 10.0),
+                    //   padding: EdgeInsets.fromLTRB(10.0, 10, 10, 0.0),
+                    //   decoration: BoxDecoration(
+                    //     color: Colors.white,
+                    //     border: Border(
+                    //       top: BorderSide(color: Colors.grey, width: 0.5),
+                    //     ),
+                    //   ),
+                    //   child: Row(
+                    //     children: [
+                    //       Container(
+                    //         padding: EdgeInsets.all(5),
+                    //         child: Icon(MdiIcons.facebook,
+                    //             color: Colors.blue, size: 30),
+                    //       ),
+                    //       Container(
+                    //         padding: EdgeInsets.all(5),
+                    //         child: Icon(MdiIcons.web,
+                    //             color: Colors.blue, size: 30),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // )
                   ],
                 ),
               ),
